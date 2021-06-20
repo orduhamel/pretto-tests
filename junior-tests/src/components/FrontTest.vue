d133084a70f00df9601c1e8abe94e35451ebdd24
F-Hameau <f.hameau@gmail.com>
2021-06-20 19:24:48 +0200

    add-git-repo-link

--- junior-tests/src/components/FrontTest.vue
+++ junior-tests/src/components/FrontTest.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
     <h1>Implémenter une popup</h1>
-    <p>Du back et du front pour tester ta technique</p>
+    <p>
+      Pour commencer, tu vas devoir "forker" ce site depuis le repo
+      <a href="https://github.com/F-Hameau/pretto-tests" target="_blank">Github</a>
+    </p>
+    <p>
+      Ensuite, on code ! Le but ici est d'afficher le contenu de l'input ci-dessous dans une popup au clique sur le
+      bouton
+    </p>
+    <div>
+      <input class="large" type="text" placeholder="Texte à mettre dans la popup" />
+    </div>
+    <div>
+      <button class="large">Afficher la popup</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "FrontTest"
+  name: "FrontTest",
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+input {
+  font-family: "MaisonNeue-Medium";
+  font-size: 16px;
+  margin-bottom: 24px;
+  padding: 15px 0px;
+  text-align: center;
+}
+</style>
